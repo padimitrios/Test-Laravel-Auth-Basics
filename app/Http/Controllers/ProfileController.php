53b7b93d51bce1ca51e40f301d3d9b16550379e7
Dimitris Papathanasiou <dimitrispapathanasioy00@gmail.com>
2024-09-03 09:31:34 +0300

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\ProfileUpdateRequest;
 
 class ProfileController extends Controller
@@ -15,6 +17,18 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+
+        $user = Auth::user();
+
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+
+        if ($request->input('password')) {
+            $new_password = bcrypt($request->input('password'));
+            $user->password = $new_password;
+        }
+        
+        $user->save();
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
