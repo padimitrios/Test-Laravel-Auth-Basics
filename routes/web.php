8f99c79c80bca012b9dd3ea7e3328862c7a979da
Dimitris Papathanasiou <dimitrispapathanasioy00@gmail.com>
2024-09-03 09:29:46 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,24 +13,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
 
-Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', function () {
+        return view('home');
+    })->name('home');
 
-// Task: profile functionality should be available only for logged-in users
-Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
-Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
 
-// Task: this "/secretpage" URL should be visible only for those who VERIFIED their email
-// Add some middleware here, and change some code in app/Models/User.php to enable this
-Route::view('/secretpage', 'secretpage')
-    ->name('secretpage');
+    // Task: profile functionality should be available only for logged-in users
+    Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
+    Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
 
-// Task: this "/verysecretpage" URL should ask user for verifying their password once again
-// You need to add some middleware here
-Route::view('/verysecretpage', 'verysecretpage')
-    ->name('verysecretpage');
+    // Task: this "/secretpage" URL should be visible only for those who VERIFIED their email
+    // Add some middleware here, and change some code in app/Models/User.php to enable this
+    Route::view('/secretpage', 'secretpage')
+        ->name('secretpage')
+        ->middleware('verified');
+
+    // Task: this "/verysecretpage" URL should ask user for verifying their password once again
+    // You need to add some middleware here
+    Route::view('/verysecretpage', 'verysecretpage')
+        ->name('verysecretpage')->middleware(['password.confirm']);;
+
+    Route::get('/confirm-password', function () {
+        return view('auth.confirm-password');
+    })->middleware('auth')->name('password.confirm');
+
+    Route::post('/confirm-password', function (Request $request) {
+        if (! Hash::check($request->password, $request->user()->password)) {
+            return back()->withErrors([
+                'password' => ['The provided password does not match our records.']
+            ]);
+        }
+     
+        $request->session()->passwordConfirmed();
+     
+        return redirect()->intended();
+    })->middleware(['auth', 'throttle:6,1']);
+});
 
 require __DIR__.'/auth.php';
